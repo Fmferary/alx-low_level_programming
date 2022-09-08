ac4463a150e80242f5959c52d56803f7a003e540
Olufemi Adegboyega <fmferary@gmail.com>
2022-09-08 11:55:35 -0700

    my c level low programming

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ *
+ *  *main - entry point
+ *
+ *   *
+ *
+ *    *Return: 0 after printing the function
+ *
+ *     */
+
+int main(void)
+
+{
+
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+
+}
+
+Footer
+
+© 2022 GitHub, Inc.
+
+Footer navigation
+
+Terms
